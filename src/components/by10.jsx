e5690e63d0ab8f152089a38e3131e9d13db1e856
SparshKhanna0001 <65919066+SparshKhanna0001@users.noreply.github.com>
2025-07-20 20:26:13 +0530

    Fix: fav section fixed. Closes #2. added another video, nd some styolig to support it

--- src/components/by10.jsx
+++ src/components/by10.jsx
@@ -1,16 +1,22 @@
 import videoFile from "../assets/veo-video.mp4"
+import anakinVideo from "../assets/Anakin_vs_Palpatine_Video.mp4"
+import "../styles/by10.css"
 
 function by10() {
     return(
         <div>
 
+        <h4 style={{ color: "#f5f5f5", marginLeft: "13%" }}>Star Wars Video</h4>
 
-        <video height='50%' width='50%' controls >
-            <source src={videoFile} type="video/mp4"/>
+        <video height='50%' width='50%' controls className="center" >
+            <source src={anakinVideo} type="video/mp4"/>
             the video aint playing kido!
         </video>
 
-
+        <video height='50%' width='50%' controls className="center" >
+            <source src={videoFile} type="video/mp4"/>
+            the video aint playing kido!
+        </video>
         </div>
     );
 }
--- src/pages/favourites.jsx
+++ src/pages/favourites.jsx
@@ -1,13 +1,12 @@
 import '../styles/Favorites.css'
 import {useMovieContext} from '../context/MovieContext'
 import MovieCard from '../components/movieUI';
-//import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Favourites() {
 
     const {favorites} = useMovieContext();
-
-    if(favorites)
+    if(!favorites.length === 0){
     return (
         <div className='favorites'>        
         <h2 className='favorites-h2'>Favorties</h2>
@@ -19,15 +18,15 @@ function Favourites() {
             </div>
         </div>
 
-    );
-
+    )}
+    else{
     return (
         <div className='favorites-empty'>
             <h2 className='favorites-h2'>No Favorites </h2>
-            <p className='favorites-empty-p'>Add favourites by clicking on the heart icon on each moviecard from home page.</p>
+            <p className='favorites-empty-p'>Add favourites by clicking on the heart icon on each moviecard from <Link to="/"> home </Link> page.</p>
         </div>
 
-    );
+    );}
 
 }
 
